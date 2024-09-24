1e5715010f15587fc8388eaed600a421e14dda4b
SerhiiKurlykin <kurlykin83@gmail.com>
2024-09-24 14:46:58 +0200

    Add files via upload

---
+++ Homework_1/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int number = 347;
+
+
+        System.out.print("Число " + number + " -> ");
+        System.out.print(" 3 ");
+        System.out.print(" 4 ");
+        System.out.println(" 7 ");
+    }
+}
+
---
+++ Homework_1/src/homework2/homework2.java
@@ -0,0 +1,18 @@
+package homework2;
+
+public class homework2 {
+
+    public static void main(String[] args) {
+
+
+        int number = 345;
+
+
+        System.out.print("Число " + number + " -> ");
+        System.out.print(" 3 ");
+        System.out.print(" 4 ");
+        System.out.println(" 5 ");
+    }
+}
+
+
